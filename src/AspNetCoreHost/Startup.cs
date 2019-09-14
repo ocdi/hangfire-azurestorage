6527b09ebee74a31f6ffd088f14cb82f56117e7c
Trevor Nichols <trevor@theredfoxgroup.com.au>
2019-09-14 20:36:44 +1000

    Add AspNetCoreHost project

---
+++ src/AspNetCoreHost/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.AzureStorage;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace AspNetCoreHost
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.TryAddSingleton<AzureStorageOptions>(new AzureStorageOptions
+            {
+                ConnectionString = "UseDevelopmentStorage=true;",
+                Prefix = "Sample"
+            });
+            services.AddHangfire((provider, configuration) => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseAzureStorage(provider.GetRequiredService<AzureStorageOptions>()));
+
+            
+            services.AddHangfireServer(options =>
+            {
+                options.StopTimeout = TimeSpan.FromSeconds(15);
+                options.ShutdownTimeout = TimeSpan.FromSeconds(30);
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHangfireDashboard();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
